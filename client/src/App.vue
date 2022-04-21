0900be2acd74b137e2433b084c7f1f6bed373c80
Dzmitry Shvabovich <dgs@anadeainc.com>
2022-04-21 10:56:23 +0300

    update email verify component

--- client/src/App.vue
+++ client/src/App.vue
@@ -26,8 +26,9 @@
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap');
 
-  body {
+  * {
     margin: 0;
+    box-sizing: border-box;
     font-family: 'Montserrat', sans-serif;
   }
 </style>
