16a04a8c678ebe6d1ffce9204fe04b02487a52a0
Dzmitry Shvabovich <dgs@anadeainc.com>
2022-05-12 11:56:07 +0300

    add stripe webhook

--- server/service/stripe.service.js
+++ server/service/stripe.service.js
@@ -2,6 +2,9 @@ const stripe = require('stripe')(
   'sk_test_51Kw01FFiR2H5b1l17TNRgiEeHGBmKA4R7iyDf0htdYkXbT1kOmcLHUIl5h4wCxeRoZy8yOZb9whT5JKCBB2IlDFK00CEP3CenX'
 )
 
+const endpointSecret =
+  'whsec_ca9fdaac4398780d2099054a953f25513cab1ab8efff461b208d55dca0418271'
+
 class StripeService {
   async pay(paymentDetails) {
     const price = await stripe.prices.create({
@@ -23,6 +26,21 @@ class StripeService {
     })
 
     return session
+  }
+
+  async webhook(payload, sig) {
+    const event = stripe.webhooks.constructEvent(payload, sig, endpointSecret)
+
+    switch (event.type) {
+      case 'payment_intent.succeeded': {
+        return event.data.object
+      }
+      case 'payment_method.attached': {
+        return event.data.object
+      }
+      default:
+        console.log(`Unhandled event type ${event.type}.`)
+    }
   }
 }
 
