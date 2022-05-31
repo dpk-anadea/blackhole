e546787313565cc5350c2966230557fdbff3e853
dpk <dpk@anadeainc.com>
2022-05-31 16:13:35 +0300

    FIX: test

--- client/components/modals/__test__/PayModal.spec.js
+++ client/components/modals/__test__/PayModal.spec.js
@@ -3,19 +3,14 @@ import { createNewStore } from '@/lib/test-helpers'
 
 import PayModal from '@/components/modals/PayModal'
 import BaseModal from '@/components/modals/BaseModal'
+import productFactory from '@factory/product.factory'
 
 describe('PayModal component', () => {
   const store = createNewStore()
   const wrapper = mount(PayModal, {
     global: { plugins: [store] },
     props: {
-      products: [
-        {
-          id: 1,
-          name: 'Product-1',
-          description: 'test@mail.ru'
-        }
-      ],
+      products: [productFactory.attrs('product')],
       totalCost: 100
     },
     stubs: { BaseModal }
