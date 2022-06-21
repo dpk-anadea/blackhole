3b1cc2adec9f60aabb9353f10ee14564157f34b6
dpk <dpk@anadeainc.com>
2022-06-21 16:18:19 +0300

    test

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const { sequelize } = require('./server/models')
 
 const PORT = process.env.PORT || process.env.APP_PORT || 3030
 
+app.get('*', function (req, res) {
+  // eslint-disable-next-line no-undef
+  res.send(path.join(__dirname, './public/index.html'))
+})
+
 app.listen(PORT, async () => {
   console.log(`Server started on port ${PORT}`)
   await sequelize.authenticate()
   console.log('Database Connected!')
 })
-
-app.get('/*', (req, res) => res.send('Index Page'))
