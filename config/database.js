0f867865ce0dc04ec8bd701dcbdf6d1cfbee5b63
dpk <dpk@anadeainc.com>
2022-06-21 10:01:42 +0300

    FIX: database

--- config/database.js
+++ config/database.js
@@ -25,5 +25,23 @@ module.exports = {
       underscored: true
     },
     logging: false
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    define: {
+      timestamps: true,
+      underscored: true
+    },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 }
